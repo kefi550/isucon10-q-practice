00ac91044302f53b8189c193e46bf4f27fc338a8
kefi550 <ytiryrstd550@gmail.com>
2026-09-05 12:17:37 +0900

    push.sh

---
+++ push.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -eu
+
+LOCAL_PATH="."
+
+for i in $(seq 1); do
+  host="isu${i}"
+  echo $host
+  rsync -avr "${LOCAL_PATH}/webapp/go/" ${host}:~/isuumo/webapp/go/ 
+  # rsync -avr "${LOCAL_PATH}/env.sh" ${host}:~/env.sh
+  rsync -avr "${LOCAL_PATH}/webapp/mysql/" ${host}:~/isuumo/webapp/mysql/ 
+  ssh ${host} 'bash -l -c "sudo logrotate -f /etc/logrotate.conf; export PATH=/home/isucon/local/go/bin:/home/isucon/go/bin:\$PATH; cd ~/isuumo/webapp/go; rm isuumo; make; sudo systemctl restart isuumo.go.service;"'
+done
+
